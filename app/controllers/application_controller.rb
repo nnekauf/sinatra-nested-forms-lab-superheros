951fb78dc12d944126e13930a59bf6eec807ded7
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-07 06:17:37 -0500

    commit tree 8416596ca46d3f90846720c3c746aa773937190a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,22 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    
+    end 
 
+    post '/teams' do
+        
+      @team = Team.new(params[:team])
+      members = params[:team][:members]
+      members.each do |hero|
+        SuperHero.new(hero)
+      end
+        
+    
+
+      @heros = SuperHero.all
+      erb :team
+    end 
 end
